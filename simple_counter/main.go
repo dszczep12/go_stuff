0250949dc64af3fb4b58882eac2b0325edee09cc
Dylan Szczepanski <adorevi9@gmail.com>
2024-05-06 15:10:57 -0500

    added another command

--- simple_counter/main.go
+++ simple_counter/main.go
@@ -2,31 +2,40 @@ package main
 
 import (
 	"bufio" // read text
-	"fmt"  // printing stuff 
-	"io" // read from io	
-	"os" // use os resources?
 	"flag"
+	"fmt" // printing stuff
+	"io"  // read from io
+	"os"  // use os resources?
 )
 
-func count(r io.Reader, countLines bool) int {
-	scanner := bufio.NewScanner(r)
-	
+func count(r io.Reader, countLines bool, countBytes bool) int {
+    var scanner *bufio.Scanner
 
-	if(!countLines) {
-		scanner.Split(bufio.ScanWords)
-	}
+    if countLines {
+        scanner = bufio.NewScanner(r)
+    } else {
+        scanner = bufio.NewScanner(r)
+        scanner.Split(bufio.ScanWords)
+    }
 
-	wc:= 0
+    if countBytes {
+        scanner = bufio.NewScanner(r)
+        scanner.Split(bufio.ScanBytes)
+    }
 
-	for scanner.Scan() {
-		wc++
-	}
-	
-	return wc;
+    wc := 0
+
+    for scanner.Scan() {
+        wc++
+    }
+
+    return wc
 }
+
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	line_bytes := flag.Bool("b", false, "count bytes")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))	
+	fmt.Println(count(os.Stdin, *lines, *line_bytes))
 }
--- simple_counter/main_test.go
+++ simple_counter/main_test.go
@@ -8,17 +8,25 @@ import (
 func TestCountLines(t *testing.T) {
   b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
   exp := 3
-    res := count(b, true)
+    res := count(b, true, false)
   if res != exp {
   t.Errorf("Expected %d, got %d instead.\n", exp, res)
   } }
+
+  func TestCountBytes(t *testing.T) {
+    b := bytes.NewBufferString("hjsuowpdhj")
+    exp := 10
+      res := count(b, false, true)
+    if res != exp {
+    t.Errorf("Expected %d, got %d instead.\n", exp, res)
+    } }
 
 // TestCountWords test the count function set to count TestCountWords
 func TestCountWords(t *testing.T) {
   b := bytes.NewBufferString("word1 word2 word3 word4\n")
 
   exp := 4
-  res := count(b, false)
+  res := count(b, false, false)
   if res != exp {
     t.Errorf("Expected %d, got %d instead.\n", exp, res)
   }
